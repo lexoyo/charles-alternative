40e378f3d5d91fe7bdddcfaad6d15fc6d1a67ead
Alex Hoyau <a.hoyau@silexlabs.org>
2016-10-18 19:44:10 +0200

    regexp and a list of files to map to local

--- index.js
+++ index.js
@@ -13,14 +13,20 @@ var fs = require('fs');
 
 var proxy = httpProxy.createProxyServer({});
 var server = http.createServer(function(req, res) {
-  console.log('Asked:', req.url);
-  if(req.url.indexOf('player.7385a0c48f299495e73b.js') > 0) {
-    console.log('REPLACED: player.7385a0c48f299495e73b.js');
+  const substitutions = [
+    {from: 'player.7385a0c48f299495e73b.js', to: './player.7385a0c48f299495e73b.js'},
+    {from: 'http://.*\.gooseelysima.com/.*\.swf', to: './ping.swf'},
+    {from: 'http://.*/crossdomain\.xml', to: './crossdomain.xml'},
+  ];
+  const found = substitutions.find(item => req.url.match(new RegExp(item.from)));
+  if(found) {
+    console.log('REPLACED', req.url, 'matches', found.from, '=>', found.to);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(fs.readFileSync('./player.7385a0c48f299495e73b.js'));
+    res.write(fs.readFileSync(found.to));
     res.end();
   }
   else {
+    console.log('served as is', req.url);
     proxy.web(req, res, { target: req.url + '?' });
   }
 });
